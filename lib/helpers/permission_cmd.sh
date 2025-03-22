9fe08086877555effaca03e51b1892506298cde9
Aria Stewart <aredridel@dinhe.net>
2025-03-21 21:47:52 -0400

    fix

--- lib/helpers/permission_cmd.sh
+++ lib/helpers/permission_cmd.sh
@@ -1,15 +1,15 @@
 permission_cmd () {
   case $1 in
-    Linux) echo "stat --printf '%a'" ;;
-    Darwin|FreeBSD) echo "stat -f '%Lp'" ;;
+    Linux) echo "stat --printf %a" ;;
+    Darwin|FreeBSD) echo "stat -f %Lp" ;;
     *) return 1 ;;
   esac
 }
 
 permission_cmd_dir () {
   case $1 in
-    Linux) echo "stat --printf '%U\n%G\n%a'" ;;
-    Darwin|FreeBSD) echo "stat -f '%Su%n%Sg%n%Lp'" ;;
+    Linux) echo "stat --printf %U\n%G\n%a" ;;
+    Darwin|FreeBSD) echo "stat -f %Su%n%Sg%n%Lp" ;;
     *) return 1 ;;
   esac
 }
